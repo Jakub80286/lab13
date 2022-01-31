c49a608ae377775c30188df1235f4f134dc3cddb
Jakub80286 <80653109+Jakub80286@users.noreply.github.com>
2022-01-31 16:52:45 +0100

    Update laby13.cpp

--- laby13.cpp
+++ laby13.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char** argv){
     #pragma omp parallel
     {
-        printf("Hello from process: %d\n”, omp_get_thread_num());
+        printf("Hello from process: %d\n", omp_get_thread_num());
     }
     return 0;
 }
